8a62fde6200737ad74f1f6f6b86c090c032e435c
Jack Frysinger <frysinger.j@husky.neu.edu>
2017-07-06 17:15:50 -0400

    replace connection string

--- test/app.js
+++ test/app.js
@@ -10,8 +10,9 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
+    connectionString += '@ds151062.mlab.com:51062/heroku_fz7dl6fp'; // user yours
 }
+return connectionString;
 // Replace "@ds157268.mlab.com:57268/heroku_nh37fqq4"
 // above with your own URL given to you by mLab
 
