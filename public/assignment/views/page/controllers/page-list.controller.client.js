9614a002b54ae2773e639851bfffcf76d7298958
Jack Frysinger <frysinger.j@husky.neu.edu>
2017-07-23 20:02:08 -0400

    page controllers

---
+++ public/assignment/views/page/controllers/page-list.controller.client.js
@@ -0,0 +1,9 @@
+(function () {
+    angular
+        .module("WebAppMaker")
+        .controller("PageListController", PageListController);
+
+    function PageListController() {
+
+    }
+})();
---
+++ public/assignment/views/page/controllers/page-new.controller.client.js
@@ -0,0 +1,9 @@
+(function () {
+    angular
+        .module("WebAppMaker")
+        .controller("NewPageController", NewPageController);
+
+    function NewPageController() {
+
+    }
+})();
