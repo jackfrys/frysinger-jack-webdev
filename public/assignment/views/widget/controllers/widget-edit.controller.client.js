938ef557256ec69cbcdfe4e07b986e18af337d9e
Jack Frysinger <frysinger.j@husky.neu.edu>
2017-07-23 20:04:38 -0400

    widget controllers

---
+++ public/assignment/views/widget/controllers/widget-edit.controller.client.js
@@ -0,0 +1,9 @@
+(function () {
+    angular
+        .module("WebAppMaker")
+        .controller("EditWidgetController", EditWidgetController);
+
+    function EditWidgetController() {
+
+    }
+})();
