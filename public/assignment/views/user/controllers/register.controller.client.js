748587adaa9cfd47a8d71e8da5c44867140421f8
Jack Frysinger <frysinger.j@husky.neu.edu>
2017-07-25 23:18:20 -0400

    creating things

--- public/assignment/views/user/controllers/register.controller.client.js
+++ public/assignment/views/user/controllers/register.controller.client.js
@@ -3,7 +3,14 @@
         .module("WebAppMaker")
         .controller("RegisterController", RegisterController);
 
-    function RegisterController($scope) {
+    function RegisterController($scope, UserService, $location) {
         var vm = this;
+        vm.createUser = createUser;
+        vm.user = {"_id" : Math.round(Math.random() * 1000000)}
+
+        function createUser() {
+            UserService.createUser(vm.user);
+            $location.path("/user/" + vm.user._id);
+        }
     }
 })();
--- public/assignment/views/website/controllers/website-new.controller.client.js
+++ public/assignment/views/website/controllers/website-new.controller.client.js
@@ -3,12 +3,20 @@
         .module("WebAppMaker")
         .controller("NewWebsiteController", NewWebsiteController);
 
-    function NewWebsiteController($routeParams, WebsiteService) {
+    function NewWebsiteController($location, $routeParams, WebsiteService) {
         var vm = this;
         vm.userId = $routeParams["uid"];
+        vm.newWebsite = newWebsite;
+        vm.website = {};
         function init() {
             vm.websites = WebsiteService.findWebsitesByUser(vm.userId);
         }
         init();
+
+        function newWebsite(website) {
+            website._id = Math.round(Math.random() * 100000);
+            WebsiteService.createWebsite(vm.userId, website);
+            $location.path("/user/" + vm.userId + "/website");
+        }
     }
 })();
