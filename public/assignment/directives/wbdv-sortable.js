2943c176693d151ef2b64dd221043afff6a1379e
Jack Frysinger <frysinger.j@husky.neu.edu>
2017-08-02 22:42:50 -0400

    moved directive out

--- public/assignment/directives/wbdv-sortable.js
+++ public/assignment/directives/wbdv-sortable.js
@@ -1,3 +1,17 @@
-// $(function () {
-//     $('#sortable').remove();
-// });
+(function () {
+    angular
+        .module("WebAppMakerDirectives", [])
+        .directive("SortableWidgets", $(function () {
+            $('#sortable').sortable({
+                start: function (edit, ui) {
+                    $(this).attr("start", ui.item.index());
+                },
+                update: function (edit, ui) {
+                    var pageId = window.location.href.split("/")[10];
+                    var url = "/page/" + pageId + "/widget?initial=" + $(this).attr("start") + "&final=" + ui.item.index();
+                    $(this).removeAttr("start");
+                    $.ajax({type: "PUT", url: url})
+                }
+            });
+        }));
+})
