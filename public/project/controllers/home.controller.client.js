3acb3afe20005723a8cc9e0d65db15bf2c325e4c
Jack Frysinger <frysinger.j@husky.neu.edu>
2017-08-03 12:24:58 -0400

    search

--- public/project/controllers/home.controller.client.js
+++ public/project/controllers/home.controller.client.js
@@ -3,10 +3,17 @@
         .module("MBTASafe")
         .controller("HomeController", HomeController);
 
-    function HomeController($http) {
+    function HomeController($http, $location) {
         var vm = this;
+        vm.submit = submit;
+        vm.search = "";
+
         $http.get("/api/stops").then(function (resp) {
             vm.stops = resp.data;
-        })
+        });
+
+        function submit() {
+            $location.path(vm.search);
+        }
     }
 })();
