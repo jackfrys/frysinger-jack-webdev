11f3011dc171a5336ae0dc47eb809324c33a7bf4
Jack Frysinger <frysinger.j@husky.neu.edu>
2017-08-14 21:21:54 -0400

    moving protytyopes

--- public/project/config.js
+++ public/project/config.js
@@ -4,26 +4,6 @@
         .config(Config);
 
     function Config($routeProvider) {
-        $routeProvider
-            .when("/", {
-                templateUrl: "views/login.view.client.html",
-                controller: "LoginController",
-                controllerAs: "model"
-            })
-            .when("/home", {
-                templateUrl: "views/home.view.client.html",
-                controller: "HomeController",
-                controllerAs: "model"
-            })
-            .when("/:stopId", {
-                templateUrl: "views/stop.view.client.html",
-                controller: "StopController",
-                controllerAs: "model"
-            })
-            .when("/user/:uid", {
-                templateUrl: "views/user.view.client.html",
-                controller: "UserController",
-                controllerAs: "model"
-            })
+        
     }
 })();
