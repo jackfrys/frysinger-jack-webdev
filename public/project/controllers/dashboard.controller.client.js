f7952d6f7310d8eb1284c6c1821bc830869bfbeb
Jack Frysinger <frysinger.j@husky.neu.edu>
2017-08-15 23:51:06 -0400

    traveler dashboard

--- public/project/controllers/dashboard.controller.client.js
+++ public/project/controllers/dashboard.controller.client.js
@@ -7,5 +7,6 @@
         var vm = this;
 
         vm.user = {role: "TRAVELER"};
+        vm.routes = [{name: "My route 1", steps: [{place: "place-nuniv", step: "EXIT"}], origin: "Back Bay Station", destination: "Northeastern University"}]
     }
 })();
