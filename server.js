ebc899e2cfb1a29ba6cc46c9b9f9ca89e0303c01
Jack Frysinger <frysinger.j@husky.neu.edu>
2017-12-31 23:45:27 -0500

    reformatted server

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 var app = require('./express');
 
-var bodyParser    = require('body-parser');
-var multer        = require('multer');
-var passport      = require('passport');
-var cookieParser  = require('cookie-parser');
-var session       = require('express-session');
-var mongoose      = require('mongoose');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var mongoose = require('mongoose');
 
 var express = app.express;
 
 app.set('view engine', 'ejs');
-// require("./utilities/filelist");
 
 app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 multer();
 app.use(session({
     secret: 'this is the secret',
